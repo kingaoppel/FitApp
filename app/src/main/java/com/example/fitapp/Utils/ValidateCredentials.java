7dd99267b7f0517286e189275e10498ef301d05c
xNikus <comlokoxd@gmail.com>
2022-11-03 20:03:44 +0100

    Create Validation class

---
+++ app/src/main/java/com/example/fitapp/Utils/ValidateCredentials.java
@@ -0,0 +1,16 @@
+package com.example.fitapp.Utils;
+
+import java.util.regex.Pattern;
+
+public class ValidateCredentials {
+    private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+    private static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$";
+
+    public static boolean matchesEmail(String email) {
+        return Pattern.compile(EMAIL_REGEX).matcher(email).matches();
+    }
+
+    public static boolean matchesPassword(String password) {
+        return Pattern.compile(PASSWORD_REGEX).matcher(password).matches();
+    }
+}
