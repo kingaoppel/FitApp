cf28ee83081f50b87c5b1bf6b0747b52bd177490
20MiWi00 <65978856+20MiWi00@users.noreply.github.com>
2022-11-03 17:30:25 +0100

    Adding SplashScreen to the project

--- app/src/main/java/com/example/fitapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/fitapp/SplashScreenActivity.java
@@ -2,7 +2,10 @@ package com.example.fitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        if(getSupportActionBar() != null)
+            getSupportActionBar().hide();
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent toLoginActivity = new Intent(SplashScreenActivity.this,LoginActivity.class);
+                SplashScreenActivity.this.finish();
+                startActivity(toLoginActivity);
+            }
+        }, 2600);
+
     }
 }
