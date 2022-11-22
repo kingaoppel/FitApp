dfd1c70bda5ff7194991f3b8210cb831f8984c4b
kingaoppel <228819@edu.p.lodz.pl>
2022-11-22 21:56:43 +0100

    Basic elements of the product addition view

---
+++ app/src/main/java/com/example/fitapp/AddProducktActivity.java
@@ -0,0 +1,14 @@
+package com.example.fitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddProducktActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_produckt);
+    }
+}
